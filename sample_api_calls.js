bfe48cdf953fc6a67d5a8e760fde5c89210c28e0
Samuel Cabral <samfcabral@gmail.com>
2014-12-14 19:27:27 -0800

    home page almost there

--- sample_api_calls.js
+++ sample_api_calls.js
@@ -15,3 +15,20 @@ request('http://www.zillow.com/webservice/GetZestimate.htm?zws-id=<"X1-ZWz1dzv5l
   	console.log(body) 
   }
 })
+
+Below is an example of calling the API for the address for the exact address match "2114 Bigelow Ave", "Seattle, WA": 
+http
+
+request('http://www.zillow.com/webservice/GetZestimate.htm?zws-id=<ZWSID>&zpid=X1-ZWz1dzv5l0hkaz_6h6e1', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+  	console.log(body) 
+  }
+})
+
+
+//Below is a sample call to the API for zpid 48749425:
+//http://www.zillow.com/webservice/GetZestimate.htm?zws-id=<ZWSID>&zpid=X1-ZWz1dzv5l0hkaz_6h6e1
+
+//http://www.zillow.com/webservice/GetSearchResults.htm
+
+// "http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=<X1-ZWz1dzv5l0hkaz_6h6e1>&address=" + address + "citystatezip="+ citystatezip;
