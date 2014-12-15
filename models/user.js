8705216631e3dcd6310ca00f9355a01c6e66dfa3
Samuel Cabral <samfcabral@gmail.com>
2014-12-15 12:16:32 -0800

    added user database and migrated

---
+++ models/user.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define("user", {
+    email: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    phone: DataTypes.INTEGER,
+    password_digest: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+
+  return user;
+};
