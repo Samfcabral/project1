282d6b82e92a44e3d820b6b987467cc637a5119f
Samuel Cabral <samfcabral@gmail.com>
2015-04-09 17:22:10 -0700

    cashflow

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ app.use(express.static(__dirname + '/public'));
 //End of dependencies
 
 //Session
-app.use(session( {
+app.use(session({
   secret: 'thisismysecretkey',
   name: 'chocolate chip',
   // this is in milliseconds
   maxage: 3600000
-  })
-);
+})
+  );
 
 // get passport started
 app.use(passport.initialize());
@@ -33,7 +33,7 @@ app.use(passport.session());
 SERIALizING
 Turns relevant user data into a string to be stored as a cookie
 */
-passport.serializeUser(function(user, done){
+passport.serializeUser(function (user, done) {
   console.log("SERIALIZED JUST RAN!");
   done(null, user.id);
 });
@@ -43,19 +43,19 @@ DeSERIALizing
 Taking a string and turns into an object
   using the relevant data stored in the session
 */
-passport.deserializeUser(function(id, done){
+passport.deserializeUser(function (id, done) {
   console.log("DESERIALIZED JUST RAN!");
   db.user.find({
-      where: {
-        id: id
-      }
-    })
-    .then(function(user){
+    where: {
+      id: id
+    }
+  })
+    .then(function (user) {
       done(null, user);
     },
-    function(err) {
-      done(err, null);
-    });
+      function (err) {
+        done(err, null);
+      });
 });
 
 // HOME PAGE
@@ -66,7 +66,7 @@ app.get("/", function (req, res) {
 
 // WHEN SOMEONE WANTS THE SIGNUP PAGE
 app.get("/register", function (req, res) {
-   if (!req.user) {
+  if (!req.user) {
     res.render("users/register", {currentUser: req.user});
   } else {
     res.redirect("/", {currentUser: req.user});
@@ -74,12 +74,12 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/users/:id", function (req, res) {
-  console.log("USERSSS SHOW")
+  console.log("USERSSS SHOW");
   req.user.getWatchlists().then(function (list) {
     res.render("users/profile", {watchlist: list});
-  })
-
+  });
 });
+
 // WHEN SOMEONE  SUBMITS A SIGNUP PAGE
 app.post("/users", function (req, res) {
   console.log("POST /users");
@@ -94,18 +94,18 @@ app.post("/users", function (req, res) {
     function (err, user) {
       // when successfully created log the user in
       // req.login is given by the passport
-      req.login(user, function(){
+      req.login(user, function () {
         // after login redirect to home
-        console.log("Id: ", user.id)
+        console.log("Id: ", user.id);
         res.redirect("/");
       });
-    })
+    });
 });
 
 // WHEN SOMEONE WANTS THE LOGIN PAGE
 app.get("/login", function (req, res) {
-    if (req.user) {
-      console.log("App Login Get");
+  if (req.user) {
+    console.log("App Login Get");
     res.redirect("/");
   } else {
     console.log("App Login Get Else");
@@ -119,36 +119,23 @@ app.post('/login', passport.authenticate('local', {
   failureRedirect: '/login'
 }));
 
-// app.get("/", function (req, res) {
-//   console.log(req.user)
-//   // req.user is the user currently logged in
-
-//   if (req.user) {
-//     res.render("sites/home", {user: req.user});
-//   } else {
-//     res.render("sites/home", {user: false});
-//   }
-// });
-
+// Logout User
 app.get("/logout", function (req, res) {
   // LOG OUT
   req.logout();
   res.redirect("/");
 });
 
-
-
 // Create a handler to respond to GET requests
 // to our search page ("/search").
-app.get('/search', function(req,res) {
-    var address = req.query.address;
-    var city = req.query.city;
-    var state = req.query.state;
-    var zip = req.query.zip;
-    var params = { address: address, city: city, state: state, zip: zip}
-    
-    zillow.getDeepSearchResults(params)
-    .then(function(results) {
+app.get('/search', function (req, res) {
+  var address = req.query.address;
+  var city = req.query.city;
+  var state = req.query.state;
+  var zip = req.query.zip;
+  var params = { address: address, city: city, state: state, zip: zip};
+  zillow.getDeepSearchResults(params)
+    .then(function (results) {
       console.log("RECEIVING RESULTS");
       try {
         var price = results.valueOf().response[0].results[0].result[0].zestimate[0].amount[0]._;
@@ -165,13 +152,34 @@ app.get('/search', function(req,res) {
         console.log(soldDate);
         var soldPrice = results.valueOf().response[0].results[0].result[0].lastSoldPrice[0]._;
         console.log(soldPrice);
-        console.log("PARAMS", params.address)
-        res.render('users/show', {currentUser: req.user, params: params, amount:price, bedrooms:numRooms, bathrooms:bathrooms, sqft:sqft, year:year, soldDate: soldDate, soldPrice:soldPrice});
+        console.log("PARAMS", params.address);
+        res.render('site/show', {currentUser: req.user, params: params, amount: price, bedrooms: numRooms, bathrooms: bathrooms, sqft: sqft, year: year, soldDate: soldDate, soldPrice: soldPrice});
       } catch (error) {
         res.redirect("/fail");
       }
     });
 });
+
+// app.get("/cashFlow", function (req, res) {
+//   var findPayment = function (principal, interest, years) {
+//     var payment = (principal * interest) / (1 - Math.pow(1 + interest, -years));
+//     console.log("this is my payment ", payment);
+//     return payment;
+//   };
+
+//   var principal = req.query.principal;
+//   var term = req.query.years;
+//   var interest = req.query.interest;
+//   var monthlyPayment = findPayment(principal, interest / 12, term * 12);
+//   var taxes = req.query.taxes;
+//   var hoa = req.query.hoa;
+//   var insurance = req.query.insurance;
+//   var rent = req.query.rent;
+//   var totalCashflow = rent - monthlyPayment - taxes - hoa - insurance;
+
+//   console.log("This should be my " + monthlyPayment);
+//   console.log(totalCashflow);
+// });
 
 app.post("/watchlist", function (req, res) {
   if (req.user) {
@@ -179,20 +187,18 @@ app.post("/watchlist", function (req, res) {
       street: req.body.watchlist.street,
       userId: req.user.id
     }).then(function (watchlist) {
-      res.redirect("/users/"+req.user.id);
-    })
-  } else { 
+      res.redirect("/users/" + req.user.id);
+    });
+  } else {
     res.redirect("/");
   }
 });
-
-
 
 app.get("/fail", function (req, res) {
   res.render("site/fail");
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync().then(function () {
   var server = app.listen(process.env.PORT || 3000, function() {
     console.log(new Array(51).join("*"));
     console.log("\t LISTENING ON: \n\t\t localhost:3000");
