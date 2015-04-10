cdc748b3ef6677b288c445a2cdb86709b960a26f
Samuel Cabral <samfcabral@gmail.com>
2015-04-09 17:22:10 -0700

    cashflow

---
+++ public/javascripts/calculate.js
@@ -0,0 +1,26 @@
+$(function() {
+  var findPayment = function (principal, interest, years) {
+      var payment = (principal * interest) / (1 - Math.pow(1 + interest, -years));
+        //console.log("this is my payment ", payment);
+      return payment;
+  };
+
+  var cashFlow = function(obj) {
+    var monthly = findPayment(obj.principal, obj.interest/12, obj.years * 12);
+    console.log(monthly);
+    var cf = obj.rent - monthly - obj.hoa - obj.tax - obj.insurance;
+     console.log("" + obj.rent + " - " + monthly + " - " + obj.hoa + " - " + obj.tax + " - " + obj.insurance);
+    return cf;
+  };
+
+ $("form.cf").on( "submit", function( event ) {
+  event.preventDefault();
+  var obj = {};
+  $( this ).serializeArray().forEach(function(el,id,arr){
+    obj[el.name] = Number(el.value);
+  });
+  $(".result").html(cashFlow(obj));
+
+ });
+
+});
